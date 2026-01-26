b90587efe319999548a54d6ac75f27f178e5b46f
Ravikmrp <ravikumarkmrp@gmail.com>
2026-01-26 18:25:21 +0000

    changes the instance type

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2_create" {
   ami           = "ami-0532be01f26a3de55"# Amazon Linux 2 (us-east-1)
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = "Ravi_aws"
 
   tags = {
