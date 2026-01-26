85aed171eec04bb72343ab527e122b99eb4169a4
root <root@ip-172-31-84-89.ec2.internal>
2026-01-26 18:00:13 +0000

    added all the value

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "ec2_create" {
+  ami           = "ami-0532be01f26a3de55"# Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+  key_name      = "Ravi_aws"
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
+
